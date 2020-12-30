e4047d037bab5fd9cf37d80c2178eff46ef365d2
respawn_harada <harada@respawn.tokyo>
2020-12-30 22:26:24 +0900

    create branch multiple-states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-const App = () => {
-  return (
-    <div>
-      This is a template for React app.
+import React, { useState } from 'react'
 
-    </div>
+const App = (props) => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+  return (
+    <>
+      <p>現在の{name}は、{price}です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>+1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} type='text' onChange={e =>　setName(e.target.value)}/>
+    </>
   );
 }
 
+// 初期化
+App.defaultProps = {
+  name: 'サンプル',
+  price: 1000
+}
 export default App;
