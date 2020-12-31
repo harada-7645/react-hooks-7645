7586d09549de2aa9b0498a28e1ae528e6b9e227e
respawn_harada <harada@respawn.tokyo>
2020-12-31 12:09:28 +0900

    refactor components

--- src/components/App.js
+++ src/components/App.js
@@ -1,63 +1,18 @@
-import React, { useState, useReducer } from 'react'
+import React, { useReducer } from 'react'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reducer from '../reducers'
-import Event from './Event'
+
+import EventForm from './EventForm'
+import Events    from './Events'
+import reducer   from '../reducers'
 
 //https://getbootstrap.com/docs/5.0/getting-started/webpack/
 const App = () => {
-  // 第2引数 初期化
-  // 第3引数 初期化時に実行したい関数を渡すことができる
   const [state, dispatch] = useReducer(reducer, [])
-  const [title, setTitle] = useState('')
-  const [body,  setBody] = useState('')
-  const addEvent = e => {
-    e.preventDefault()
-    dispatch({
-      type: 'CREATE_EVENT',
-      title,
-      body
-    })
-    setTitle('')
-    setBody('')
-  }
-
-  const deleteAllEvents = e => {
-    e.preventDefault()
-    const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if ( result ) dispatch({type: 'DELETE_ALL_EVENTS'})
-  }
-
-  const unCreatable = title === '' || body === ''
   return (
     <div className='container-fluid'>
-      <h4>イベント作成フォーム</h4>
-      <form>
-        <div className='form-group'>
-          <label htmlFor='formEventTitle'>タイトル</label>
-          <input className='form-control' id='formEventTitle' onChange={e => setTitle(e.target.value)} value={title}/>
-        </div>
-        <div className='form-group'>
-          <label htmlFor='formEventBody'>ボディー</label>
-          <textarea className='form-control' id='formEventBody' onChange={e => setBody(e.target.value)} value={body}/>
-        </div>
-        <div>
-          <button className='btn btn-primary' onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
-          <button className='btn btn-danger'  onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
-        </div>
-      </form>
-      <h4>イベント一覧</h4>
-      <table className='table table-hover'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディー</th>
-          </tr>
-        </thead>
-        <tbody>
-          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>)) }
-        </tbody>
-      </table>
+      <EventForm state={state} dispatch={dispatch}/>
+      <Events state={state} dispatch={dispatch}/>
     </div>
   );
 }
---
+++ src/components/EventForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const EventForm = ({state, dispatch}) => {
+  const [title, setTitle] = useState('')
+  const [body,  setBody] = useState('')
+  const addEvent = e => {
+    e.preventDefault()
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+    setTitle('')
+    setBody('')
+  }
+
+  const deleteAllEvents = e => {
+    e.preventDefault()
+    const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
+    if ( result ) dispatch({type: 'DELETE_ALL_EVENTS'})
+  }
+
+  const unCreatable = title === '' || body === ''
+
+  return (
+    <>
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className='form-group'>
+          <label htmlFor='formEventTitle'>タイトル</label>
+          <input className='form-control' id='formEventTitle' onChange={e => setTitle(e.target.value)} value={title}/>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='formEventBody'>ボディー</label>
+          <textarea className='form-control' id='formEventBody' onChange={e => setBody(e.target.value)} value={body}/>
+        </div>
+        <div>
+          <button className='btn btn-primary' onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
+          <button className='btn btn-danger'  onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default EventForm
