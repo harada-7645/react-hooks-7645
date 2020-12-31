fa09621da12a73272e0419114c37bd2839861231
respawn_harada <harada@respawn.tokyo>
2020-12-31 18:49:10 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,12 @@
 import React, { useState, useContext } from 'react'
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions'
+import { 
+  CREATE_EVENT, 
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_OPERATION_LOGS
+} from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext)
@@ -13,6 +19,13 @@ const EventForm = () => {
       title,
       body
     })
+
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    })
+
     setTitle('')
     setBody('')
   }
@@ -20,7 +33,14 @@ const EventForm = () => {
   const deleteAllEvents = e => {
     e.preventDefault()
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if ( result ) dispatch({type: DELETE_ALL_EVENTS})
+    if ( result ) {
+      dispatch({type: DELETE_ALL_EVENTS})
+      dispatch({
+        type: DELETE_OPERATION_LOGS,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601()
+      })
+    }
   }
 
   const unCreatable = title === '' || body === ''
