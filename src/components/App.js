9dd7b3b3cbff811d92c665c60f1898d753453570
respawn_harada <harada@respawn.tokyo>
2020-12-31 13:05:15 +0900

    react context

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,20 @@ import React, { useReducer } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import EventForm from './EventForm'
-import Events    from './Events'
-import reducer   from '../reducers'
+import EventForm  from './EventForm'
+import Events     from './Events'
+import AppContext from '../contexts/AppContext'
+import reducer    from '../reducers'
 
-//https://getbootstrap.com/docs/5.0/getting-started/webpack/
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
   return (
-    <div className='container-fluid'>
-      <EventForm state={state} dispatch={dispatch}/>
-      <Events state={state} dispatch={dispatch}/>
-    </div>
+    <AppContext.Provider value={{ state, dispatch }}>
+      <div className='container-fluid'>
+        <EventForm/>
+        <Events/>
+      </div>
+    </AppContext.Provider>
   );
 }
 export default App;
