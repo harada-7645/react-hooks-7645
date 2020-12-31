572dca94a29733019252b73c86c05a368a687d46
respawn_harada <harada@respawn.tokyo>
2020-12-31 10:55:04 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState, useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from '../reducers'
 
 //https://getbootstrap.com/docs/5.0/getting-started/webpack/
 const App = () => {
+  // 第2引数 初期化
+  // 第3引数 初期化時に実行したい関数を渡すことができる
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body,  setBody] = useState('')
+  const addEvent = e => {
+    e.preventDefault()
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+    setTitle('')
+    setBody('')
+  }
   return (
     <div className='container-fluid'>
       <h4>イベント作成フォーム</h4>
       <form>
         <div className='form-group'>
           <label htmlFor='formEventTitle'>タイトル</label>
-          <input className='form-control' id='formEventTitle' />
+          <input className='form-control' id='formEventTitle' onChange={e => setTitle(e.target.value)}/>
         </div>
         <div className='form-group'>
           <label htmlFor='formEventBody'>ボディー</label>
-          <textarea className='form-control' id='formEventBody' />
+          <textarea className='form-control' id='formEventBody' onChange={e => setBody(e.target.value)} />
         </div>
         <div>
-          <button className='btn btn-primary'>イベントを作成する</button>
+          <button className='btn btn-primary' onClick={addEvent}>イベントを作成する</button>
           <button className='btn btn-danger'>全てのイベントを削除する</button>
         </div>
       </form>
@@ -35,5 +51,4 @@ const App = () => {
     </div>
   );
 }
-
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -22,10 +22,10 @@
 
 const events = (state = [], action) => {
   switch (action.type) {
-    case 'CRETE_EVENT':
+    case 'CREATE_EVENT':
       const event = {title: action.title, body: action.body}
       const length = state.length
-      const id = (length === 0) ? 1 : stete[length - 1].id + 1
+      const id = (length === 0) ? 1 : state[length - 1].id + 1
       // return [...state, {id: id, ...event}]
       return [...state, {id, ...event}] //省略形
     case 'DELETE_EVENT':
